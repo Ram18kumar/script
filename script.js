2d89cbadd342c16c0bf1a4b7984a44fff9618abc
Ram18kumar <134294896+Ram18kumar@users.noreply.github.com>
2024-01-18 09:48:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function assign(){
+    //length
+    console.log(num.length);
+   //toString
+   console.log(num.toString());
+  
+   //arrayat
+   console.log(num[2]);
+  
+   //join
+   console.log(num.join("+"));
+  
+   //pop
+   console.log(num.pop());
+   //push
+   console.log(num.push(6));
+   //concat
+   console.log(num.concat(num1));
+   
+  
+  
+      
+  
+  }
+  const num=[1,2,3,4,5];
+  const num1=[6,7,8,9,10]
+  assign(num,num1);
